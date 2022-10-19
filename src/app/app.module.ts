d77268f6155830e3f1fdf0bbbed980a40173ac64
DaianaChile <daiarocio@outlook.com>
2022-10-18 21:49:41 -0300

    primer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './website/components/nav/nav.component';
+import { HeroComponent } from './website/components/hero/hero.component';
+import { AboutComponent } from './website/components/about/about.component';
+import { ExperienceComponent } from './website/components/experience/experience.component';
+import { ButtonComponent } from './website/components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeroComponent,
+    AboutComponent,
+    ExperienceComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
